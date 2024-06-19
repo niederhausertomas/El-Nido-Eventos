a83c0a3ab01fe940ea8c22e03442d1c4a00d124e
Tomás Niederhauser <78279996+niederhausertomas@users.noreply.github.com>
2024-06-20 01:55:59 +0200

    WIP

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var navbar = document.getElementById('navbar');
     window.addEventListener('scroll', function() {
-      if (window.scrollY > 10) {
+      if (window.scrollY > 40) {
         navbar.classList.add('scrolled');
       } else {
         navbar.classList.remove('scrolled');
