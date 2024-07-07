029837f27b9ef65a207466944e2357adfb5adcc3
Tomás Niederhauser <78279996+niederhausertomas@users.noreply.github.com>
2024-07-07 23:37:41 +0200

    WIP :)

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+
+//Formulario de contacto. formspree.io
+const $form = document.querySelector('#form')
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers:{
+            'Accept': 'application/json'
+        }
+    })
+    if(response.ok){
+        this.reset();
+        alert("Mensaje enviado!")
+    }
+}
